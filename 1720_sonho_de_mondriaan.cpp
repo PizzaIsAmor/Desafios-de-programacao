f0cdb795207b54bbf98b333766ad6606b705c525
Rafael Fernandes Borges <rafaeelfernandes@hotmail.com>
2021-04-13 15:38:42 -0300

    bugfix 1720

--- 1720_sonho_de_mondriaan.cpp
+++ 1720_sonho_de_mondriaan.cpp
@@ -6,47 +6,62 @@ using namespace std;
 
 int altura;
 int largura;
-int x;  // x = largura
-int y;  // y = altura
 int quadro[INF][INF];
 int respostas[INF + 1][INF + 1];
 
-void atualizaPosicao(int alt) {
-    for (y = alt; y < altura; ++y)
-        for (x = 0; x < largura; ++x)
-            if (quadro[x][y] == -1)
+void proximaPosicao(int *x, int *y) {
+    while ((*y) < altura) {
+        for ((*x) = 0; (*x) < largura; ++(*x))
+            if (quadro[(*x)][(*y)] == -1) {
                 return;
+            }
+
+        ++(*y);
+    }
 }
 
-int mondriaan() {
+int mondriaan(int x, int y) {
+    if (respostas[x][y] != -1)
+        return respostas[x][y];
+
     if (x == largura && y == altura)
         return 1;
 
-    int xLocal = x, yLocal = y, resp = 0;
+    int resp = 0;
 
-    if (xLocal + 1 <= largura - 1 && quadro[xLocal + 1][yLocal] == -1) {
-        quadro[xLocal][yLocal] = 1;
-        quadro[xLocal + 1][yLocal] = 1;
-        atualizaPosicao(yLocal);
+    // adiciona horizontal
+    if (x + 1 <= largura - 1 && quadro[x + 1][y] == -1) {
+        // marca quadro
+        quadro[x][y] = 1;
+        quadro[x + 1][y] = 1;
 
-        resp += mondriaan();
+        // acha proxima posicao
+        int xLocal, yLocal = y;
+        proximaPosicao(&xLocal, &yLocal);
 
-        quadro[xLocal][yLocal] = -1;
-        quadro[xLocal + 1][yLocal] = -1;
-    }
-    if (yLocal + 1 <= altura - 1) {
-        quadro[xLocal][yLocal] = 1;
-        quadro[xLocal][yLocal + 1] = 1;
-        atualizaPosicao(yLocal);
+        resp += mondriaan(xLocal, yLocal);
 
-        resp += mondriaan();
-
-        quadro[xLocal][yLocal] = -1;
-        quadro[xLocal][yLocal + 1] = -1;
+        // reseta quadro
+        quadro[x][y] = -1;
+        quadro[x + 1][y] = -1;
     }
 
-    x = xLocal;
-    y = yLocal;
+    // adiciona vertical
+    if (y + 1 <= altura - 1) {
+        //marca quadro
+        quadro[x][y] = 1;
+        quadro[x][y + 1] = 1;
+
+        // acha proxima posicao
+        int xLocal, yLocal = y;
+        proximaPosicao(&xLocal, &yLocal);
+
+        resp += mondriaan(xLocal, yLocal);
+
+        //reseta quadro
+        quadro[x][y] = -1;
+        quadro[x][y + 1] = -1;
+    }
 
     return resp;
 }
@@ -62,14 +77,20 @@ int main() {
 
     cin >> altura >> largura;
     while (altura != 0 && largura != 0) {
-        // prepara posição
-        x = 0;
-        y = 0;
-        // Calcula
-        cout << mondriaan() << endl;
+        cout << mondriaan(0, 0) << endl;
 
         cin >> altura >> largura;
     }
 
     return 0;
 }
+
+//1 2
+//1 3
+//1 4
+//2 2
+//2 3
+//2 4
+//2 11
+//4 11
+//0 0
